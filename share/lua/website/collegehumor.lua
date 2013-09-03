5ecebfa8ef28f7629ce3cf389e2716fc89005756
Ansgar Burchardt <ansgar@debian.org>
2013-09-03 21:31:17 +0200

    Imported Upstream version 0.4.18

--- share/lua/website/collegehumor.lua
+++ share/lua/website/collegehumor.lua
@@ -1,5 +1,5 @@
 
--- libquvi-scripts v0.4.17
+-- libquvi-scripts v0.4.18
 -- Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
 -- Copyright (C) 2010-2011  Lionel Elie Mamane <lionel@mamane.lu>
 --
--- share/lua/website/videa.lua
+++ share/lua/website/videa.lua
@@ -1,5 +1,5 @@
 
--- libquvi-scripts v0.4.17
+-- libquvi-scripts v0.4.18
 -- Copyright (C) 2011  Bastien Nocera <hadess@hadess.net>
 --
 -- This file is part of libquvi-scripts <http://quvi.sourceforge.net/>.
